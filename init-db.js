7899461e99bdcc5507eb9f53577c417b6a072bcc
Natalie Monged <nataliemonged20@gmail.com>
2026-04-08 20:16:54 +0200

    Trigger pipeline

--- init-db.js
+++ init-db.js
@@ -3,5 +3,6 @@ db.tasks.drop();
 db.tasks.insertMany([
   { id: 1, name: 'Setup Lab 8', status: 'completed' },
   { id: 2, name: 'Configure MongoDB', status: 'pending' },
+  
   { id: 3, name: 'Run CI/CD Pipeline', status: 'pending' }
 ]);
